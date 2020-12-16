22018dc9c9b3b5f0a242b3bad328ad7c5e631609
Taylor McLean <taylor@taylors-MBP>
2020-12-16 05:49:43 -0500

    style: add grid layout for calendar

--- src/CrossableCalendar/CrossableCalendar.tsx
+++ src/CrossableCalendar/CrossableCalendar.tsx
@@ -1,7 +1,21 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, calc(100% / 7));
+`
 
 const CrossableCalendar: FC = () => {
-  return <div>Crossable Calendar</div>
+  const arr: number[] = [...Array(10).keys()]
+
+  return (
+    <Grid>
+      {arr.map((n) => (
+        <div key={n}>{`Test ${n}`}</div>
+      ))}
+    </Grid>
+  )
 }
 
 export default CrossableCalendar
